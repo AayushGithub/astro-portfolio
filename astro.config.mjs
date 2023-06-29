784421e7a21d05e23ff6a6e17e88d4c3b01472b7
AayushGithub <aayushgandhi2001@gmail.com>
2023-06-29 16:49:27 +0530

    PreFetch Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
-  integrations: [sitemap()],
+  integrations: [sitemap(), prefetch()],
   site: 'https://aayushgandhi.com'
 });
